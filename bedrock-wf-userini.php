53a871fc751eedccbf2bd75abf72657b7e62525e
Nicolas Scott <nic@crearewebsolutions.com>
2023-10-03 17:17:37 -0400

    wordfence deploy

--- bedrock-wf-userini.php
+++ bedrock-wf-userini.php
@@ -1,5 +1,34 @@
 <?php
 
+
+
+
+//Check if the wordfence file exists in the wo directory
+//echo __DIR__ . 'web/wp/wordfence-waf.php';
+
+if ( file_exists( __DIR__ . '/web/wp/wordfence-waf.php') ) {
+
+	$wordfence_waf = file_get_contents( __DIR__ . '/web/wp/wordfence-waf.php');
+
+	$wordfence_waf = str_replace( '../app', 'app', $wordfence_waf );
+
+	if( file_put_contents( __DIR__ . '/web/wordfence-waf.php', $wordfence_waf ) ){
+		echo $wordfence_waf;
+		echo "The Wordfence WAF was written.\n";
+	} else {
+
+		echo "The Wordfence WAF was not written.\n";
+	
+	}
+} else {
+ 
+echo "There was an error.\n";
+
+}
+
+
+
+
 $userini = file_get_contents( __DIR__ . '/web/.user.ini' );
 
 if ( empty( $userini) ) {
@@ -28,3 +57,5 @@ if ( file_put_contents( __DIR__ . '/web/.user.ini', $new_userini ) ) {
 	
 	echo 'The file was not written.';
 }
+
+
